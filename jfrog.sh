af13d84d363120d70e1a8ec4c84bbd0d358bbd2c
Learndevops-083 <113036991+learndevops-083@users.noreply.github.com>
2023-08-17 15:08:09 +0100

    Update jfrog.sh

--- jfrog.sh
+++ jfrog.sh
@@ -5,11 +5,14 @@ sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker $(whoami)
-
+sudo usermod -aG docker ec2-user
+sudo mkdir -p /jfrog/artifactory
+sudo chown -R 1030:1030 /jfrog/
 # Run JFrog Artifactory Docker container with volumes and restart policy
-sudo docker run -d \
-  --name jfrog-artifactory \
+#pull jfrog docker image
+sudo docker pull docker.bintray.io/jfrog/artifactory-oss:latest
+
+sudo docker run --name artifactory -d -p 8081:8081 -p 8082:8082 \
   --restart unless-stopped \
-  -p 8081:8081 -p 8082:8082 \
-  docker.bintray.io/jfrog/artifactory-pro:v1.0.38
+  -v /jfrog/artifactory:/var/opt/jfrog/artifactory \
+ docker.bintray.io/jfrog/artifactory-oss:latest
